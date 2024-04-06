c0444d18fa12811a23303867bbddc1cc08b7a5c5
GrCOTE7 <grcote7@gmail.com>
2024-04-06 16:10:37 +0200

    Feat: Add new blade directive: @adminOrOwner

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,30 @@
 <?php
 
+/**
+ * (ɔ) GrCOTE7 - 1990-2024
+ */
+
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void
+	{
+		Blade::if('adminOrOwner', function ($id) {
+			return auth()->check() && (auth()->id() === $id || auth()->user()->admin);
+		});
+	}
 }
