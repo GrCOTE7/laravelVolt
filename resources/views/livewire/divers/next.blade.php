b8eb416519cb9f646b929ca1f2a6b5a3f6a55869
GrCOTE7 <grcote7@gmail.com>
2024-04-13 17:54:43 +0200

    Feat: Assets in a component

--- resources/views/livewire/divers/next.blade.php
+++ resources/views/livewire/divers/next.blade.php
@@ -9,14 +9,28 @@ $v = new #[Layout('components.layouts.posts')] #[Title('Next')] class extends Co
 }; ?>
 
 <div>
-
-    @script
-        <script>
-            setInterval(() => {
-                $wire.$refresh()
-                console.log('Top...');
-            }, 2000)
-        </script>
-    @endscript
-
+    <input type="text" data-picker>
 </div>
+
+@assets
+    <script src="https://cdn.jsdelivr.net/npm/pikaday/pikaday.js" defer></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/pikaday/css/pikaday.css">
+@endassets
+
+@script
+    <script>
+        new Pikaday({
+            field: $wire.$el.querySelector('[data-picker]')
+        });
+    </script>
+@endscript
+
+@script
+    <script>
+        setInterval(() => {
+            $wire.$refresh()
+            console.log('Top...');
+        }, 2000)
+    </script>
+@endscript
+
